6745961fc785742a2acd542240fcc3fa1ea48846
yangahxu <413909880@qq.com>
2019-10-21 15:55:37 +0800

    1

---
+++ 作业/第1关作业.py
@@ -0,0 +1,17 @@
+# number1 = 1
+# number2 = 2
+# unit1 = '人'
+# unit2 = '眼'
+# line1 = '我编程累'
+# line2 = '是bug相随'
+# sentence1 = '碎掉的节操满地堆'
+# sentence2 = '我只求今日能早归'
+
+# print(str(number1)+unit1+line1+sentence1)
+# print(str(number2)+unit2+line2+sentence2)
+
+# slogan = '脸黑怪我咯'
+# number = '7.8'
+# unit = '张'
+# sentence = '蓝票一个SSR都没有'
+# print(slogan+str(int(float(number)))+unit+sentence)
---
+++ 课堂练习/第1关 萌新的进化.py
@@ -0,0 +1,51 @@
+# hero1 = '亚瑟' 
+# hero2 = '李白'
+# action = '秒掉'
+# gain = '获得'
+# achieve = 'First Blood'
+
+# print(hero1+action+hero2+gain+achieve)
+# print(hero2+action+hero1+gain+achieve)
+
+# hero = '亚瑟' 
+# enemy = '敌方'
+# action = '秒杀'
+# gain = '获得'
+# number = 5
+# achieve = 'Penta Kill'
+# print(hero+action+str(number)+enemy+gain+achieve)
+
+
+# hero = '亚瑟' 
+# enemy = '敌方'
+# action = '秒杀'
+# gain = '获得'
+# number = 5
+# achieve = 'Penta Kill'
+
+# print(type(hero))
+# print(type(enemy))
+# print(type(action))
+# print(type(gain))
+# print(type(number))
+# print(type(achieve))
+
+# bug='666'
+# hair='0'
+
+# print(int(bug)+int(hair))
+
+
+# height = 183.5
+# weight = 79
+# age = '30'
+
+# print(float(height))
+# print(float(weight))
+# print(float(age))
+
+word = '3.8'
+number = 1
+sentence = '人工智障说：3.8+1等于'
+
+print(sentence+str(int(float(word)+number)))
