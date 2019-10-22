0d25beb7419151d5d3eb6d19b18f0eb206fc9051
yangahxu <413909880@qq.com>
2019-10-22 16:54:58 +0800

    0

--- 课堂练习/第2关 灭霸的选择.py
+++ 课堂练习/第2关 灭霸的选择.py
@@ -1,5 +1,44 @@
-weight=int(input('体重等于'))
-if weight>100:
-    print('不吃了')
+# weight=int(input('体重等于'))
+# if weight>100:
+#     print('不吃了')
+# else:
+#     print('吃吃吃')
+
+# stnum=int(input('你有多少石头？'))
+
+# if stnum >= 6:
+#     print('你拥有了毁灭宇宙的力量')
+# elif 3 < stnum <= 5:
+#     print('红女巫需要亲手毁掉幻视额头上的心灵宝石')
+# else :
+#     print('需要惊奇队长逆转未来')
+
+
+# import time
+# print('一切都化为虚无')
+# time.sleep(1.5)
+# print('一切都化为尘埃')
+# time.sleep(1.5)
+# print('他们就这样随风消散')
+# time.sleep(1.5)
+# print('冬兵')
+# time.sleep(1.5)
+# print('格鲁特')
+# time.sleep(1.5)
+# print('红女巫')
+
+#分数点评
+historyscore=int(input('请输入分数：'))
+if historyscore>=60:
+    print('你已经及格')
+    if historyscore>=80:
+        print('你很优秀')
+    else:
+        print('你只是一般般')
 else:
-    print('吃吃吃')
+    print('不及格')
+    if historyscore<30:
+        print('学渣')
+    else:
+        print('还能抢救一下')
+print('程序结束')
