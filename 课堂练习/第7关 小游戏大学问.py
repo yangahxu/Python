872c0874b87a7a562f8ed6b3f7e28685fca383cb
Yangjx <yangjx@YangjxdeMacBook-Pro.local>
2019-10-26 15:54:51 +0800

    0

--- 课堂练习/第7关 小游戏大学问.py
+++ 课堂练习/第7关 小游戏大学问.py
@@ -23,6 +23,7 @@ a = 0
 player_victory = 0
 enemy_victory = 0
 for i in range(3):
+    
     time.sleep(1.5)
     a += 1
     print('\n---------------现在是第'+str(a)+'局-----------------')
@@ -30,20 +31,19 @@ for i in range(3):
     player_attack = random.randint(30,50)
     enemy_life = random.randint(100,150)
     enemy_attack = random.randint(30,50)
-    print('【玩家】\n血量：%s\n攻击：%s' % (player_life,player_attack))  # 自定义玩家角色的血量和攻击
+    print('【玩家】\n血量：%s\n攻击：%s' % (player_life,player_attack))  
     print('------------------------') 
     time.sleep(1.5)
     print('【敌人】\n血量：%s\n攻击：%s' % (enemy_life,enemy_attack))
-    print('------------------------')   # 自定义敌人角色的血量和攻击
+    print('------------------------')   
+    
     time.sleep(1.5)
-
-
     while player_life > 0 and enemy_life > 0:
         player_life = player_life - enemy_attack
         enemy_life = enemy_life - player_attack
-        print('你发起了攻击，【敌人】剩余血量%s' % (enemy_life))  # 人工计算敌人血量：100-50=50
+        print('你发起了攻击，【敌人】剩余血量%s' % (enemy_life))  
         print('敌人向你发起了攻击，【玩家】剩余血量%s' % (player_life))
-        print('------------------------')   # 人工计算玩家血量：100-30=70
+        print('------------------------')   
         time.sleep(1.5)
     if player_life > 0 and enemy_life <= 0:
         player_victory += 1
@@ -54,8 +54,8 @@ for i in range(3):
     else:
         print('哎呀，你和敌人同归于尽了！')
     print('当前比分\n【玩家：%s--敌人：%s】'% (player_victory,enemy_victory))
+time.sleep(1.5)
 if player_victory > enemy_victory :
-    time.sleep(1)
     print('【最终结果：你赢了！】')
 elif enemy_victory > player_victory:
     print('【最终结果：你输了！】')
