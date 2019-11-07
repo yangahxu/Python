42d3a548cfda79bc052b74bd703ae54107b28922
Yangjx <yangjx@YangjxdeMacBook-Pro.local>
2019-11-08 00:00:06 +0800

    0

---
+++ 课堂练习/第8关 编程学习的两大瓶颈.py
@@ -0,0 +1,21 @@
+#99乘法表
+# for i in range(1,3):   
+#     print('%d x %d = %d' % (i,2,i*2),end=' ')
+# print('')
+# for i in range(1,4):
+#     print('%d x %d = %d' % (i,3,i*3),end=' ')
+# print('')
+
+# for i in range(1,10):
+#     for j in range(1,i+1):   
+#         print('%d x %d = %d' % (j,i,j*i),end=' ')
+#     print('')
+
+i = 1
+while i <= 9: 
+    j = 1
+    while j <= i:
+        print('%d X %d = %d' % (j,i,i*j),end = '  ') 
+        j += 1
+    print('')
+    i += 1
