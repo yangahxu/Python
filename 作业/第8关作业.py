43e08c2f517e672f5e54961e8731fb6ce96fd3a0
Yangjx <yangjx@YangjxdeMacBook-Pro.local>
2019-11-09 15:16:21 +0800

    0

--- 作业/第8关作业.py
+++ 作业/第8关作业.py
@@ -1,5 +1,16 @@
-#作业1
+#作业1-合并列表
 list1 = [91, 95, 97, 99]
 list2 = [92, 93, 96, 98] 
 
+list3 = list1 + list2
 
+print(list3)
+list3.sort()#升序排列
+print(list3)
+
+#作业2
+s = 0
+for i in list3:
+    s = s + i
+average = s / 8
+print(' 大家的平均分是%d ' % (average))
--- 课堂练习/第8关 编程学习的两大瓶颈.py
+++ 课堂练习/第8关 编程学习的两大瓶颈.py
@@ -1,16 +1,17 @@
-#99乘法表
-# for i in range(1,3):   
-#     print('%d x %d = %d' % (i,2,i*2),end=' ')
-# print('')
-# for i in range(1,4):
-#     print('%d x %d = %d' % (i,3,i*3),end=' ')
-# print('')
+#99乘法表-一般语法
+for i in range(1,3):   
+    print('%d x %d = %d' % (i,2,i*2),end=' ')
+print('')
+for i in range(1,4):
+    print('%d x %d = %d' % (i,3,i*3),end=' ')
+print('')
+#最简洁语法
+for i in range(1,10):
+    for j in range(1,i+1):   
+        print('%d x %d = %d' % (j,i,j*i),end=' ')
+    print('')
 
-# for i in range(1,10):
-#     for j in range(1,i+1):   
-#         print('%d x %d = %d' % (j,i,j*i),end=' ')
-#     print('')
-
+#while语法示例
 i = 1
 while i <= 9: 
     j = 1
